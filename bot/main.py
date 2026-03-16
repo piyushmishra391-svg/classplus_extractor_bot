210b9ec93e270b487e9c64b8f810821822408079
piyushmishra391-svg <piyushmishra391@gmail.com>
2026-03-17 00:41:39 +0530

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -4,9 +4,9 @@ from pyrogram.types import Message
 
 bot = Client(
     'classplus_bot',
-    api_id=int(os.getenv('21567814')),
-    api_hash=os.getenv('cd7dc5431d449fd795683c550d7bfb7e'),
-    bot_token=os.getenv('8078418472:AAHN802dzlaLZX2D9g3URDTlic6W7IX0Fb4')
+    api_id=int(os.getenv('30424326')),
+    api_hash=os.getenv('eb823f3b2a0e6b7af29ac42efe701129'),
+    bot_token=os.getenv('8762678534:AAEhShFlS5vPenLLYj4MzvgDuZawaKIlT68')
 )
 
 @bot.on_message(filters.command('start'))
